8fcbf365ff7176abf17d29b244b9e0487882cd76
Ergoold <34137153+Ergoold@users.noreply.github.com>
2019-10-31 01:15:51 +0200

    added test cases for `QuantumRegister`

---
+++ Quantum/test/runtests.jl
@@ -0,0 +1,16 @@
+#=
+runtests:
+- Julia version: 1.2.0
+- Author: user
+- Date: 2019-10-31
+=#
+
+using Quantum, Test
+
+@testset "Quantum" begin
+    @testset "QuantumRegister" begin
+        @test QuantumRegister(1) == [1, 0]
+        @test QuantumRegister(2) == [1, 0, 0, 0]
+        @test QuantumRegister(3) == [1, 0, 0, 0, 0, 0, 0, 0]
+    end
+end
