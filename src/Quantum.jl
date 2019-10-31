0f777c8f3e71fdaea5d1f682b885555f189bf828
Ergoold <34137153+Ergoold@users.noreply.github.com>
2019-10-31 10:27:12 +0200

    implemented `Base.length` for `QuantumRegister`

--- src/Quantum.jl
+++ src/Quantum.jl
@@ -2,18 +2,21 @@ module Quantum
 
 using LinearAlgebra
 import Base.==
+import Base.length
 
 export QuantumRegister
 
 struct QuantumRegister
-    size::Int
     prod::Vector{Float64}
-
-    QuantumRegister(size::Int) = new(size, vcat([1], zeros(Float64, 2 ^ size - 1)))
+    QuantumRegister(length::Int) = new(vcat([1], zeros(Float64, 2 ^ length - 1)))
 end
 
 ==(register::QuantumRegister, vector::Vector) = register.prod == vector
 ==(vector::Vector, register::QuantumRegister) = vector == register.prod
 ==(a::QuantumRegister, b::QuantumRegister) = a.prod == b.prod
+
+# Because `QuantumRegister` saves the tensor product of all its qubits,
+# its length is 2^n (where n is the amount of qubits)
+length(register::QuantumRegister) = Int(log2(length(register.prod)))
 
 end # module
