7fde2bda1489123c79c7a17c090afa1edb4598cc
Ergoold <34137153+Ergoold@users.noreply.github.com>
2019-10-31 10:50:29 +0200

    changed unclear property names

--- src/Quantum.jl
+++ src/Quantum.jl
@@ -6,16 +6,16 @@ import Base: ==, length
 export QuantumRegister
 
 struct QuantumRegister
-    prod::Vector{Float64}
+    qubit_product::Vector{Float64}
     QuantumRegister(length::Int) = new(vcat([1], zeros(Float64, 2 ^ length - 1)))
 end
 
-==(register::QuantumRegister, vector::Vector) = register.prod == vector
-==(vector::Vector, register::QuantumRegister) = vector == register.prod
-==(a::QuantumRegister, b::QuantumRegister) = a.prod == b.prod
+==(register::QuantumRegister, vector::Vector) = register.qubit_product == vector
+==(vector::Vector, register::QuantumRegister) = vector == register.qubit_product
+==(a::QuantumRegister, b::QuantumRegister) = a.qubit_product == b.qubit_product
 
 # Because `QuantumRegister` saves the tensor product of all its qubits,
 # its length is 2^n (where n is the amount of qubits)
-length(register::QuantumRegister) = Int(log2(length(register.prod)))
+length(register::QuantumRegister) = Int(log2(length(register.qubit_product)))
 
 end # module
